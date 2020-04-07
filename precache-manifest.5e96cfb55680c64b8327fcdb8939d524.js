c15fbd684986b8cd7770f7fa0ef2d18f94bea136
neelamchawla <neelamchawla55@gmail.com>
2020-04-07 18:32:50 +0530

    Updates

---
+++ precache-manifest.5e96cfb55680c64b8327fcdb8939d524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe4f5ecb2e90a5866cf56cde17fdd58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6ce631e984a5e9fd23",
+    "url": "/static/css/main.05f21a9b.chunk.css"
+  },
+  {
+    "revision": "2157de2f92a0ea7c9af5",
+    "url": "/static/js/2.c92c2fa6.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "/static/js/2.c92c2fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6ce631e984a5e9fd23",
+    "url": "/static/js/main.8acc1604.chunk.js"
+  },
+  {
+    "revision": "153ac8c2b322c7a8780b",
+    "url": "/static/js/runtime-main.aa99df5f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
