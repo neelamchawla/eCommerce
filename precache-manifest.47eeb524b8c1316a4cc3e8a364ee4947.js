50b811a23fb4a5c422dc20abbd2ad1d236e39dd7
neelamchawla <neelamchawla55@gmail.com>
2020-04-09 20:01:34 +0530

    Updates

---
+++ precache-manifest.47eeb524b8c1316a4cc3e8a364ee4947.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c490c3e22e9e9a03769872660c8275a",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "299fe35d20624f79348e",
+    "url": "/ecommerce/static/css/main.9c3d1aa8.chunk.css"
+  },
+  {
+    "revision": "97db3ddf0ba006984ecb",
+    "url": "/ecommerce/static/js/2.c3404067.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "/ecommerce/static/js/2.c3404067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299fe35d20624f79348e",
+    "url": "/ecommerce/static/js/main.8cd072c0.chunk.js"
+  },
+  {
+    "revision": "87ced28b51c6e79f74bc",
+    "url": "/ecommerce/static/js/runtime-main.54de1d03.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
