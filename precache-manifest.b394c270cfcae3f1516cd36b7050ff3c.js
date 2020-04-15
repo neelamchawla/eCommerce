b366434f50296ffe231ad4d1f5e08812b080e25b
neelamchawla <neelamchawla55@gmail.com>
2020-04-15 21:17:34 +0530

    Updates

--- precache-manifest.b394c270cfcae3f1516cd36b7050ff3c.js
+++ precache-manifest.b394c270cfcae3f1516cd36b7050ff3c.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6719d7f9155eff941bad1300464fdbae",
+    "revision": "15a93df9a4a000582f8c0674d4ef8595",
     "url": "/ecommerce/index.html"
   },
   {
-    "revision": "e516a2287edd7b9c7bd7",
-    "url": "/ecommerce/static/css/main.71186412.chunk.css"
+    "revision": "af13d7ca17c6a2887668",
+    "url": "/ecommerce/static/css/main.9e386d34.chunk.css"
   },
   {
-    "revision": "3efd0e7498d160cbeaf5",
-    "url": "/ecommerce/static/js/2.8b0734ad.chunk.js"
+    "revision": "1e978de5e4eeb0181c68",
+    "url": "/ecommerce/static/js/2.295e9312.chunk.js"
   },
   {
     "revision": "0f59aded427042d87c15d0910a90fb3b",
-    "url": "/ecommerce/static/js/2.8b0734ad.chunk.js.LICENSE.txt"
+    "url": "/ecommerce/static/js/2.295e9312.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e516a2287edd7b9c7bd7",
-    "url": "/ecommerce/static/js/main.0c881799.chunk.js"
+    "revision": "af13d7ca17c6a2887668",
+    "url": "/ecommerce/static/js/main.128bf681.chunk.js"
   },
   {
     "revision": "87ced28b51c6e79f74bc",
     "url": "/ecommerce/static/js/runtime-main.54de1d03.js"
+  },
+  {
+    "revision": "4a698a515c00fbe16005e7d7b9781941",
+    "url": "/ecommerce/static/media/back.4a698a51.png"
   },
   {
     "revision": "ed28accb7dd58a0469bff983503ff07e",
